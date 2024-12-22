9ad901cb967fbfff2d8466f92b3f924677388680
m1haww <ozunmihai5@gmail.com>
2024-12-22 20:06:18 +0200

    final_final

--- lib/pages/splash_ecran.dart
+++ lib/pages/splash_ecran.dart
@@ -12,47 +12,64 @@ class _SplashEcranState extends State<SplashEcran> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 1), () async {
+    Future.delayed(const Duration(seconds: 3), () async {
       if (mounted) {
         Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const BarNavigare(),
-            ));
+          context,
+          MaterialPageRoute(
+            builder: (context) => const BarNavigare(),
+          ),
+        );
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xffFCF596),
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: const Color(0xffFCF596),
       body: SafeArea(
         child: Align(
-          alignment: Alignment.topCenter,
+          alignment: Alignment.center,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              SizedBox(
+                height: height * 0.1,
+              ),
+              // Animation for the text
+              TweenAnimationBuilder(
+                tween: Tween<double>(begin: -500.0, end: 0.0),
+                duration: const Duration(seconds: 2),
+                curve: Curves.easeInOut,
+                builder: (context, double value, child) {
+                  return Transform.translate(
+                    offset: Offset(value, 0), // Animate horizontal position
+                    child: const Text(
+                      "W E L C O M E  T O  T H E  ",
+                      style: TextStyle(
+                        color: Color(0xff85790E),
+                        fontFamily: "Italia",
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  );
+                },
+              ),
+              SizedBox(
+                height: height * 0.08,
+              ),
               Padding(
-                padding: EdgeInsets.only(top: 100.0),
-                child: Text(
-                  "W E L C O M E  T O  T H E  ",
-                  style: TextStyle(
-                      color: Color(0xff85790E),
-                      fontFamily: "Italia",
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold),
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Image.asset(
+                  'images/The_Star_Logo.png', // Replace with your actual image path
+                  width: 200, // Adjust size as needed
+                  height: 200, // Adjust size as needed
                 ),
               ),
-              Text(
-                "S T A R",
-                style: TextStyle(
-                    color: Color(0xffC4B319),
-                    fontFamily: "Italia",
-                    fontSize: 34,
-                    fontWeight: FontWeight.bold),
-              )
             ],
           ),
         ),
